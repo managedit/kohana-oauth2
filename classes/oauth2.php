6de569402c8317afdd542e9558076581ef012681
Kiall Mac Innes <kiall@managedit.ie>
2011-08-20 23:37:15 +0100

    Cleanup Controller_OAuth2_Endpoints, Its more of an example than a ready made controller now.

---
+++ classes/oauth2.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ *
+ *
+ * @package    OAuth2
+ * @category   Library
+ * @author     Managed I.T.
+ * @copyright  (c) 2011 Managed I.T.
+ */
+class OAuth2 {
+
+}
